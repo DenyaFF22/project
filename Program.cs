5c416692fc6e6f76bb6a4e64ce82158111a22ca8
DenyaFF22 <den.fed.2000@mail.ru>
2021-11-28 22:55:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 50;
+            int c = a + b; // 60
+            Console.WriteLine("a=10");
+            Console.WriteLine("b=50");
+            Console.WriteLine("c=a+b");
+            Console.WriteLine($"c={a+b}");
+        
+        }
+    }
+}
